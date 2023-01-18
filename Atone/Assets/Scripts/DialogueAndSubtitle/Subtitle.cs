74b339633e61e0b79e6baf9ea60310f7732dff6b
Thomas-DaPrato <daprato.thomas05@gmail.com>
2023-01-18 08:14:50 +0100

    fix sous-titres

--- Atone/Assets/Scripts/DialogueAndSubtitle/Subtitle.cs
+++ Atone/Assets/Scripts/DialogueAndSubtitle/Subtitle.cs
@@ -7,15 +7,15 @@ using System;
 
 public class Subtitle : Singleton<Subtitle>
 {
-
+    public GameObject subtitleText;
     public IEnumerator LaunchSubtitle(string whichDialogue){
         StreamReader reader = new StreamReader("Assets/Resources/Dialogues/" + whichDialogue + "/" + whichDialogue + ".txt");
         string line;
         while((line = reader.ReadLine()) != null){
             string[] subtitle = line.Split(new string[] { " | " }, StringSplitOptions.None);
-            gameObject.GetComponent<TextMeshProUGUI>().text = subtitle[0];
+            subtitleText.GetComponent<TextMeshProUGUI>().text = subtitle[0];
             yield return new WaitForSeconds(float.Parse(subtitle[1]));
         }
-        gameObject.GetComponent<TextMeshProUGUI>().text = "";
+        subtitleText.GetComponent<TextMeshProUGUI>().text = "";
     }
 }
