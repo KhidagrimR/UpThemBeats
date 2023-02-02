338d092b37315707079c9969f810a95f181cd96b
Thomas-DaPrato <daprato.thomas05@gmail.com>
2023-02-02 09:49:16 +0100

    ajout affichage score en fin de séquence sur UI

--- Atone/Assets/Scripts/Utility/Checkpoint.cs
+++ Atone/Assets/Scripts/Utility/Checkpoint.cs
@@ -7,7 +7,7 @@ public class Checkpoint : MonoBehaviour
     public void OnTriggerEnter(Collider other){
         if (other.CompareTag(PlayerManager.PLAYER_TAG)){
             PlayerController.checkpoint = gameObject.transform.position;
-            Debug.Log("score de la séquence " + PlayerManager.score);
+            StartCoroutine(DisplayScore.Instance.DisplayScoreWhenPlay(PlayerManager.score));
         }
             
     }
