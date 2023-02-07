cb2db67756f2b17adf24ad20cc1e4c90b390921f
DESKTOP-CKD71D6\Utilisateur <tristan.charial@gmail.com>
2023-02-07 17:31:52 +0100

    Camera shake Update bank for build

---
+++ Atone/Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using System;
+
+public class CameraManager : Singleton<CameraManager>
+{
+    [Serializable]
+    public class CameraEffect
+    {
+        public CinemachineImpulseSource impulseEffectRef;
+        public enum EffectType {
+            Explosion, Bump, Recoil
+        }
+        public EffectType effectType;
+        public float impulseStr = 5f;
+    }
+
+    public List<CameraEffect> cameraEffects = new List<CameraEffect>();
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ShakeCamera(CameraEffect.EffectType.Explosion);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ShakeCamera(CameraEffect.EffectType.Bump);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ShakeCamera(CameraEffect.EffectType.Recoil);
+        }
+    }
+
+    public void ShakeCamera(CameraEffect.EffectType effect)
+    {
+        CameraEffect cameraEffect = cameraEffects.Find(x => x.effectType == effect);
+        if(cameraEffect != null)
+        {
+            Debug.Log("<color=red>Impulse</color>");
+            cameraEffect.impulseEffectRef.GenerateImpulse(cameraEffect.impulseStr);
+        }
+    }
+}
