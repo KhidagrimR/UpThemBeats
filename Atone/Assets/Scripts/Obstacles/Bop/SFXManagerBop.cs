afe69aa9e40f2ec9df16de0411ee61755a9ad0b4
Thomas-DaPrato <daprato.thomas05@gmail.com>
2023-02-10 09:28:54 +0100

    sfx drone

---
+++ Atone/Assets/Scripts/Obstacles/Bop/SFXManagerBop.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class SFXManagerBop : MonoBehaviour
+{
+    public StudioEventEmitter arrivalSound;
+    public StudioEventEmitter destructionSound;
+    public StudioEventEmitter loadDataSound;
+    public StudioEventEmitter validationDataSound;
+}
