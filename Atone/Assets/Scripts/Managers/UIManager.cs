801f982a5610ea6382129ad73f2d0988cde9453a
tristanwaluigi@orange.fr <tristanwaluigi@orange.fr>
2023-02-15 02:21:31 +0100

    Setup for build

--- Atone/Assets/Scripts/Managers/UIManager.cs
+++ Atone/Assets/Scripts/Managers/UIManager.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class UIManager : Singleton<UIManager>
 {
     public GameObject mainMenuLanding;
+    public GameObject menuCamera;
 
     public void SetupUIGame()
     {
         mainMenuLanding.SetActive(false);
+        menuCamera.SetActive(false);
     }
 }
