8b579e33a8c5f5fedb5b01a82163213a9ddab597
Thomas-DaPrato <daprato.thomas05@gmail.com>
2023-02-15 11:32:12 +0100

    fin leaderboard + ajout input player name

--- Atone/Assets/Scripts/UI/PlayerName.cs
+++ Atone/Assets/Scripts/UI/PlayerName.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.IO;
 
 public class PlayerName : Singleton<PlayerName>
 {
     public GameObject container;
     public GameObject textError;
     
-    [InspectorReadOnly]
-    public string name;
 
     public void DisplayName(string name) {
-        this.name = name;
-        print(name);
+        if (Directory.Exists(Checkpoint.path))
+            print("directory exist !!");
+        string[] files = Directory.GetFiles(Checkpoint.path, "*.txt");
+        print("files length : " + files.Length);
+        bool isSameName = false;
+        foreach (string file in files){
+            print("file : " + file);
+            if (name == file.Split("\\")[1].Split("_")[0]){
+                isSameName = true;
+                textError.SetActive(true);
+            }
+        }
+        if (!isSameName){
+            Checkpoint.WriteScoreToFile(name);
+            Checkpoint.WriteLeaderBoard();
+            container.SetActive(false);
+        }
+            
     }
 
 
