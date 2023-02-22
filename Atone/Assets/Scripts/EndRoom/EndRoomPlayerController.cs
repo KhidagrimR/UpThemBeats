f0b8ed5f80029c8f0063fee6bed498e7fe52e21e
Thomas-DaPrato <daprato.thomas05@gmail.com>
2023-02-22 08:56:46 +0100

    fps controller fini

--- Atone/Assets/Scripts/EndRoom/EndRoomPlayerController.cs
+++ Atone/Assets/Scripts/EndRoom/EndRoomPlayerController.cs
@@ -2,20 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
+public class EndRoomPlayerController : MonoBehaviour
+{
+    public CharacterController characterController;
 
-public class EndRoomPlayerController : MonoBehaviour {
+    public float speed = 12f;
+
+    public float gravity = -9.81f;
+
+    public float jumpHeigt = 3f;
+
+    public Transform groundCheck;
+    public float groundRadiusCheck = 0.4f;
+    public LayerMask layerMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundRadiusCheck, layerMask);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
 
 
-    public float speedRotation;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-    public void Start() {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+        Vector3 move = transform.right * x + transform.forward * z; 
 
-    public void Update() {
-        if(Input.GetAxis("Mouse X") != 0)
-            transform.rotation = Quaternion.Euler(0, transform.rotation.y + Input.GetAxis("Mouse X") * speedRotation, 0);
+        characterController.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeigt * -2f * gravity);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
     }
 }
