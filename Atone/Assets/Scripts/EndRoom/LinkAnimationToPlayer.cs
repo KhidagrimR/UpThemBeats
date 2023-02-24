9be423cf0db3a02a97b0ef6c1dbb1d9a7c34d076
Zorskel <loicthibaut@sfr.fr>
2023-02-24 04:11:34 +0100

    La fin d'une ère

--- Atone/Assets/Scripts/EndRoom/LinkAnimationToPlayer.cs
+++ Atone/Assets/Scripts/EndRoom/LinkAnimationToPlayer.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class LinkAnimationToPlayer : MonoBehaviour
 {
-    public void setIsFinalDoorAnimation() {
+    public EndRoomCameraController cameraController;
+
+    public StudioEventEmitter redwall1;
+
+    public StudioEventEmitter redwall2;
+
+    public StudioEventEmitter redwall3;
+    public void setIsFinalDoorAnimation()
+    {
         EndRoomPlayerController.inAnimationFinalDoor = true;
         EndRoomPlayerController.canMove = false;
     }
 
-    public void setWakeUpEndAnimation() {
+    public void setWakeUpEndAnimation()
+    {
         EndRoomPlayerController.canMove = true;
+        cameraController.SetToPlayerView();
+    }
+
+    public void RoomWall1()
+    {
+        redwall1.Play();
+    }
+    public void RoomWall2()
+    {
+        redwall2.Play();
+    }
+    public void RoomWall3()
+    {
+        redwall3.Play();
     }
 }
